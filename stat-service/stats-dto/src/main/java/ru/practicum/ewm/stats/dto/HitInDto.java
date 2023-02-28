34dfe98f22ed0ceaad4da09556bf95cb5823d6ad
Timur <akhmetzyanovtimur3@yandex.ru>
2023-02-28 19:43:44 +0500

    fixed dto time format

--- stat-service/stats-dto/src/main/java/ru/practicum/ewm/stats/dto/HitInDto.java
+++ stat-service/stats-dto/src/main/java/ru/practicum/ewm/stats/dto/HitInDto.java
@@ -1,9 +1,11 @@
 package ru.practicum.ewm.stats.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import ru.practicum.ewm.stats.utils.DateTimeFormatPattern;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,7 +15,9 @@ import java.util.List;
 @Getter
 @Setter
 public class HitInDto {
+    @JsonFormat(pattern = DateTimeFormatPattern.TIME_PATTERN)
     private LocalDateTime start;
+    @JsonFormat(pattern = DateTimeFormatPattern.TIME_PATTERN)
     private LocalDateTime end;
     private List<String> uri;
     private boolean unique;
